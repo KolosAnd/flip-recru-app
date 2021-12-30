0bc2b9daa5075fc4b5a2b6dc15c0927fda967a87
Grygorii Nakonechnyi <nakan88@gmail.com>
2021-12-30 16:49:38 +0200

    init routing logic

--- pages/planets/[id].tsx
+++ pages/planets/[id].tsx
@@ -1,10 +1,60 @@
 import type { NextPage } from 'next'
 import {useRouter } from 'next/router';
+import Link from 'next/link'
+import axios from "axios";
+import https from "https";
 
-const Planet: NextPage = () => {
+const Planet: NextPage = ({data}: any) => {
   const router = useRouter();
   const { id } = router.query;
-  return <h1>Planet info page {id}</h1>
+  return (<>
+    <h1>Planet info page {data.name}</h1>
+    <Link href="/planets">
+      <a>back to the list</a>
+    </Link>
+  </>)
 }
 
 export default Planet;
+
+export async function getStaticProps({params}: any) {
+  const agent = new https.Agent({
+    rejectUnauthorized: false,
+   });
+   console.log("params", params);
+  const result: any = await axios.get(`https://swapi.dev/api/planets/${params?.id}`, {httpsAgent: agent});
+
+  if (!result) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: { data: result.data }, // will be passed to the page component as props
+  }
+}
+
+export async function getStaticPaths({params}: any) {
+  const agent = new https.Agent({
+    rejectUnauthorized: false,
+   });
+  const result: any = await axios.get("https://swapi.dev/api/planets", {httpsAgent: agent});
+  if (!result) {
+    return {
+      notFound: true,
+    }
+  }
+  // console.log("test", result.data);
+  const paths = result.data.results?.map((item: any, index: number) => ({
+    params: {
+      id: index.toString()
+    }
+  }))
+
+  return {
+    paths,
+    fallback: false
+  }
+
+}
--- pages/planets/index.tsx
+++ pages/planets/index.tsx
@@ -1,7 +1,48 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
+import Link from 'next/link'
+import axios from "axios";
+import https from "https";
 
-const Planets: NextPage = () => {
-  return <h1>Planets list page</h1>
+const Planets: NextPage = ({items}: any) => {
+
+ 
+
+  return <div>
+    <h1>Planets list page</h1>
+    {!!items?.length &&
+    <ul>
+      {items.map((item: any) => (<li key={item.name}>
+          <Link href={`/planets/${getPlanetId(item)}`} >
+            <a>{item.name}</a>
+          </Link>
+        </li>))}
+    </ul>
+    }
+  </div>
+}
+
+function getPlanetId (card:any) {
+  let cardLinkIndex = card.url.slice(30);
+  cardLinkIndex = cardLinkIndex.slice(0, -1);
+  return cardLinkIndex;
 }
 
 export default Planets;
+
+export async function getStaticProps(context: any) {
+  const agent = new https.Agent({
+    rejectUnauthorized: false,
+   });
+  const result: any = await axios.get("https://swapi.dev/api/planets", {httpsAgent: agent});
+
+  if (!result) {
+    return {
+      notFound: true,
+    }
+  }
+  const { data } = result;
+
+  return {
+    props: { items: data.results }, // will be passed to the page component as props
+  }
+}
